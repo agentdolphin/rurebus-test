066739bfbf97591dfc92f3716b603950498f5ff1
Mikhail Akulov <akulov@rain.ifmo.ru>
2022-05-03 00:53:05 +0300

    Batch size fix

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ with open(os.path.join(EXAMPLES_PATH, EXAMPLES_FILE), 'w', encoding='utf-8') as 
         part_examples = examples_generator.get_tag_and_relation_dataset(RUREBUS_DATA_DIR, part)
         examples.extend(part_examples)
     writer.write(json.dumps(examples))
+
+RUREBUS_TEST_DATA_DIR = 'test_data'
+RUREBUS_TEST_DATA_PARTS = ['test_full']
+EXAMPLES_TEST_FILE = 'test.json'
+
+examples = []
+with open(os.path.join(EXAMPLES_PATH, EXAMPLES_TEST_FILE), 'w', encoding='utf-8') as writer:
+    for part in RUREBUS_TEST_DATA_PARTS:
+        part_examples = examples_generator.get_tag_and_relation_dataset(RUREBUS_TEST_DATA_DIR, part)
+        examples.extend(part_examples)
+    writer.write(json.dumps(examples))
